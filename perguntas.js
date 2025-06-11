a3139116d4af100a29b3e0664a51157b5a1a5655
hillary1608 <hillary.morais@escola.pr.gov.br>
2025-06-11 01:39:52 +0000

    erfrer

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,78 @@
 criaCartao(
-    'Programação',
-    'O que é Python?',
-    'O Python é uma linguagem de programação'
+    'Caçadores',
+    'qual o nome do caçadores do mexico',
+    'calaveras'
 )
 
 criaCartao(
-    'Geografia',
-    'Qual a capital da França?',
-    'A capital da França é Paris'
+    'Kanima',
+    'quando um kanima não e um kanima',
+    'quando e o jackson'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Hospicio',
+    'nome do hopício que tinha na serie',
+    'casa eichen'
+)
+
+
+criaCartao(
+    'Hale',
+    'quem thalia hale'
+    'casa eichen'
 )
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Ancora',
+    'quem era a ancora de scott'
+    'alisson'
+
+)
+    criaCartao(
+    'Alpha',
+    'quem era o alpha da primeira temporada'
+    'peter'
+
+)
+    criaCartao(
+    'Kanima',
+    'oque o kanima faz com a garras'
+    'paralisa as pessoas'
+
+
+)
+    criaCartao(
+    'Charada',
+    'quando uma porta não e uma porta'
+    'ela esta estre aberta'
+
+
+)
+    criaCartao(
+    'Charada',
+    'todo mundo tem mais ninguem perde'
+    'a sombra'
+
+
+)
+    criaCartao(
+    'Familia',
+    'quem colocou fogo na casa da familia hale'
+    'kate argent'
+
+)
+    criaCartao(
+    'Temporada',
+    'qual foi a ultima temporada de alisson'
+    '3 temporada'
+
+
+
+
+)
+    criaCartao(
+    'Lider',
+    'quem era o lider da alcateia de alphas'
+    'deucalião'
 )
